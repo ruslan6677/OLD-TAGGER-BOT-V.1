9dc06aa9ec151dc8c8aaf5cb5727807626dde3f6
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-09-03 20:03:55 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -152,65 +152,6 @@ async def cancel(event):
   anlik_calisan.remove(event.chat_id)
 	
 	
-@client.on(events.NewMessage(pattern="^/all ?(.*)"))
-async def mentionall(event):
-  global anlik_calisan
-  if event.is_private:
-    return await event.respond("__Bu əmir yalnız qruplar üçün etibarlıdır!__")
-
-  admins = []
-  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-    admins.append(admin.id)
-  if not event.sender_id in admins:
-    return await event.respond("__Bu əmirdən yalnız idarəçilər isdifadə edə bilər!__")
-
-  if event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.reply_to_msg_id:
-    mode = "text_on_reply"
-    msg = event.reply_to_msg_id
-    if msg == None:
-        return await event.respond("__Mesajlar üşün kullanıclı tag edəmərəm!(Qrupa əlavə etmədən öncə göndərilən mesajlar)__")
-  elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("__Başlamaq üçün bir səbəb yoxdur!__")
-  else:
-    return await event.respond("__Taga başlamaq üçün səbəb yazın!__")
-
-  if mode == "text_on_cmd":
-    anlik_calisan.append(event.chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if event.chat_id not in anlik_calisan:
-        await event.respond("Tag əməliyyatı uğurla dayandırıldı!")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-	
-  if mode == "text_on_reply":
-    anlik_calisan.append(event.chat_id)
-
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if event.chat_id not in anlik_calisan:
-        await event.respond("Əməliyyat Uğurla Dayandırıldı!")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-	
-	
 @client.on(events.NewMessage(pattern="^/etag ?(.*)"))
 async def mentionall(event):
   global anlik_calisan
