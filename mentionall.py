a621de9c67b394ecd3ec53b1e9b1fb38cb6554c7
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-09-04 05:11:56 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -38,7 +38,7 @@ grup_sayi = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event): 
   await event.reply("**🤖Salam...💭,**\nMənim Adım [𝕆 𝕃 𝔻  Tag Bot](http://t.me/oldtaggerbot)-u.\n**Qurupunuz'daki  bütün üzvləri tağ etmək səlahiyyətinə sahibəm.\n\n🤖Əmrlər üçün /help yazıb məndən kömək ala bilərsiniz.**",
-  await event.reply_photo
+  await event.photo
       photo=f"{IMG_1}",
 		     buttons=(
                
