a4d5443ef4df04e51ea9a8d2384ec619216afb95
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-09-04 04:41:29 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -33,11 +33,11 @@ ozel_list = [5508658149,5354746778]
 anlik_calisan = []
 grup_sayi = []
 	
-
+(https://telegra.ph/file/cce7cc0f861e755ab775e.jpg)
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("**🤖Salam...💭,**\nMənim Adım [𝕆 𝕃 𝔻  Tag Bot](http://t.me/oldtaggerbot)-u.\n**Qurupunuz'daki  bütün üzvləri tağ etmək səlahiyyətinə sahibəm.\n\n🤖Əmrlər üçün /help yazıb məndən kömək ala bilərsiniz.**(https://telegra.ph/file/cce7cc0f861e755ab775e.jpg)",
+  await event.reply("**🤖Salam...💭,**\nMənim Adım [𝕆 𝕃 𝔻  Tag Bot](http://t.me/oldtaggerbot)-u.\n**Qurupunuz'daki  bütün üzvləri tağ etmək səlahiyyətinə sahibəm.\n\n🤖Əmrlər üçün /help yazıb məndən kömək ala bilərsiniz.**",
                     buttons=(
                
 		      [Button.url('➕ Məni Qrupa əlavə et ➕','http://t.me/oldtaggerbot?startgroup=a')],
