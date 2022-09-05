7f499df71ecefde194e6c65eee54de02c25243fd
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-09-05 08:51:01 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -86,7 +86,7 @@ emoji = "😀 🐵 🍓 😃 🦁 🍒 😄 🐯 🍎 😁 🐱 🍉 😆 🐶 �
   
 renk = "🔴 🟠 🟡 🟢 🔵 🟣 🟤 ⚫ ⚪ " .split(" ")
 
-urek = "❤️ 🧡 💛 💚 💙 💜 🖤 💘 💝 ❤️   🧡 💛 💚 💙 💜 🖤 💘 💝".split(" ")
+urek = "❤️ 🧡 💛 💚 💙 💜 🖤 💘 💝 ❤️   🧡 💛 💚 💙 💜 🖤 💘 💝" .split(" ")
 
 
 
@@ -121,7 +121,7 @@ async def mentionall(event):
     usrtxt = ""
     async for user in client.iter_participants(event.chat.id):
       usrnum += 1
-      usrtxt += f"[{random.choice(urek)}](tg://user?id={user.id")
+      usrtxt += f"[{random.choice(urek)}](tg://user?id={user.id) "
       if event.chat_id not in anlik_calisan:
         await event.respond("**Tag əməliyyatı uğurla dayandırılıdı!**")
         return
