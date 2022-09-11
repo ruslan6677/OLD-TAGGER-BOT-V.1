8a71f2d7756f2dde00e945aeb2a9869399374329
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-09-11 09:44:07 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -124,7 +124,7 @@ async def mentionall(event):
     async for user in client.iter_participants (event.chat_id):
       usrnum += 1
       usrtxt += f"[{random.choice(futbol)}](tg://user?id={user_id}) "
-      if event.chat_id not in anlik_calisan"
+      if event.chat_id not in anlik_calisan:
         await event.respond("**Tag əməliyyatı uğurla dayandırılıdı!**")
         return
       if usrnum == 5:
