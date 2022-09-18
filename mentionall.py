53530abcdfb92c89e4b37677bc2fa8e37a866e29
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-09-18 17:33:40 +0400

    update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -98,7 +98,73 @@ seherler = "Ağcabədi Ağdam Ağdaş Ağdərə Ağıstafa Ağsu Astara Babək B
 
 mafia = "👨‍🌾Vətəndaş 👨‍✈️Komissar Kattani 👨‍💼Çavuş 👨‍⚕️Doktor 🧟‍♀️Cadugar 🕵️Suiqəstçi 🧔Bomj 🦎Buqələmun 💂🏻Securıty 👳🏻‍♂️Satıcı 🙇🏻‍♂️Oğru 👷🏻‍♂️Mədənçi ⭐️General 🧝🏻‍♀️Şəhzadə 🎧DJ 🏦Bankir 🕴Don 🕺Mafia 👨‍⚖️Vəkil 🙍🏻‍♂️Məhbus 👨🏻‍🦱Dedektiv 🦦Köstəbək 👨🏻‍🎤Specialist 🔪Manyak 🤡Joker 👻Ruh 🧚🏻‍♀️Mələk 🦹🏻‍♂️BOSS 🥷Ninja 🥷SUPER Ninja 👨🏻‍🦲Dəli 🔮Reviver 💂Killer 🧛🏻‍♂️Vampir󠁧󠁢󠁷󠁬󠁳󠁿󠁧󠁢󠁷󠁬󠁳󠁿".split(" ")
 
+ad = ( "🐰 Dovşan","🦁 Şir","💍 Evli","🐶 İT","🐻 Ayı","🐭 Siçan","🥰 Sevimli","😜 Subay","😎Sevgili","👨‍👩‍👦‍👦 Ailə","🤑 Varlı","🕵‍♂ Vəkil","🐒 Meymun","🐣 Cücə","🦊 Tülkü","👩‍⚕ Həkim","👨‍🏫 Müəllim","👨‍🍳 Aşbaz","👩‍🏫 Müəllimə","🧚‍♀ Mələk","😊 Dəyərli","Gözəl💄","Çirkin😒","Unutqan 🤕","🦠 Karona","🤭 Dəcəl","😡 Lovğa","🙈Utancaq","😎 Səbirli","🧑‍🔬 Ağıllı")
 
+
+
+@client.on(events.NewMessage(pattern="^/atag ?(.*)"))
+async def mentionall(event):
+  global anlik_calisan
+  if event.is_private:
+    return await event.respond("**Bu əmir qruplar üçün etibarlıdı**")
+    
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("**Bu əmirdən yalnız idarəçilər isdifadə edə bilər!**")
+    
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+       return await event.respond("**Əvvəlki mesajlara cavab verə bilərəm!**")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+   return await event.respond("**Başlamaq üçün heç bir səbəb yoxdur! **")
+  else:
+   return await event.respond("**Tag'a başlamaq üçün səbəb yazın...!**")
+   
+  if mode == "text_on_cmd":
+    anlik_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for user in client.iter_participants (event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(ad)}](tg://user?id={user.id}) "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("**Tag əməliyyatı uğurla dayandırılıdı!**")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  if mode == "text_on_reply":
+    anlik_calisan.append(event.chat_id)
+
+    usrnum = 0
+    usrtxt = ""
+    async for user in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choic(ad)}](tg://user?id={user_id}) "
+      if event.chat_id not in anlik_calisan:
+        await event.respond("**Əməliyyat Uğurla Dayandırıldı! **")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+        
+@client.on(events.NewMessage(pattern='^(?i)/cancel'))
+async def mentionall(event):
+  global anlik_calisan 
+  anlik_calisan.remove(event.chat_id)
 
 
 @client.on(events.NewMessage(pattern="^/mtag ?(.*)"))
