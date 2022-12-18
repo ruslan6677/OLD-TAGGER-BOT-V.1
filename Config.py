624495af4159487a1a9bcbb2038fd5149b6cfd7c
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-18 13:38:19 +0400

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Config():
+    # Get these values from my.telegram.org
+    # https://my.telegram.org
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    BOT_NAME = os.environ.get("BOT_NAME")
+    BOT_ID = int(os.environ.get("BOT_ID"))
+    SUDO_USERS = os.environ.get("SUDO_USERS").split()
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT")
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    IMG_1 = os.environ.get("IMG_1", "https://telegra.ph/file/cce7cc0f861e755ab775e.jpg")
